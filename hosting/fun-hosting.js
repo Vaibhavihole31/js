e394ce13305024d4c41d7c0671451992303a693a
Vaibhavihole31 <itspinki05@gmail.com>
2022-10-20 13:25:58 +0530

    hosting practice

---
+++ hosting/fun-hosting.js
@@ -0,0 +1,19 @@
+// hello()
+
+// function hello(){
+//     console.log(a);
+// } //ReferenceError: a is not defined
+
+
+// function greet(name) {
+
+//     function displayName() {
+//         console.log('Hi' + ' ' + name);
+//     }
+
+
+//     displayName();
+// }
+// greet('John'); 
+
+
---
+++ hosting/hosting-1.js
@@ -0,0 +1,16 @@
+// var a ;
+// a = 10 
+// console.log(a);
+
+// "use strict"
+// a = 10 
+// console.log(a);
+// var a ;
+
+// var a = 10
+// console.log(a);
+
+// console.log(a);
+// const a = 10  // Refernce Error: Cannot access 'a' before initialization
+
+
